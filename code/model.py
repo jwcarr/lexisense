8780273c2523558b5be1de3683b88b6341f5df62
Jon Carr <jwcarr@users.noreply.github.com>
2021-03-08 21:24:31 +0100

    Commit initial model code

---
+++ code/model.py
@@ -0,0 +1,307 @@
+from itertools import product
+import numpy as np
+
+
+class Reader:
+
+	'''
+
+	Model reader.
+
+	'''
+
+	def __init__(self, lexicon, alpha, beta, gamma=1):
+		'''
+
+		- lexicon : A list or dictionary containing the word items. If a list, each
+		word should be a string of letters or tuple of integers, all of the same
+		length. The words are assumed to have a uniform prior. Alternatively, for a
+		non-uniform prior, a dictionary can be supplied with words as keys and
+		probabilities as values.
+
+		- alpha : A float >= 1/|S| and < 1. The alpha parameter controls the probability
+		that the reader will correctly identify the character under fixation. 
+
+		- beta : A float > 0. The beta parameter controls the rate at which the
+		probability of successful letter identification approaches chance with
+		distance from the fixation position.
+
+		- gamma : A float > 1. The gamma parameter controls how much faster the
+		probability of successful letter identification drops to the left vs. to the
+		right. If gamma is 1, the perceptual filter is symmetrical.
+		
+		'''
+		if isinstance(lexicon, dict):
+			self.lexicon = list(lexicon.keys())
+			self.lexicon_size = len(self.lexicon)
+			self.prior = np.array([lexicon[word] for word in self.lexicon])
+		elif isinstance(lexicon, list):
+			self.lexicon = lexicon
+			self.lexicon_size = len(self.lexicon)
+			self.prior = np.array([1 / self.lexicon_size] * self.lexicon_size)
+		else:
+			raise ValueError('lexicon should be of type list or dict')
+		
+		self.word_length = len(self.lexicon[0])
+		for word in self.lexicon:
+			if len(word) != self.word_length:
+				raise ValueError('All words in the lexicon must be of the same length')
+
+		if isinstance(self.lexicon[0], tuple):
+			symbols = set(sum(self.lexicon, tuple()))
+			if symbols != set(range(len(symbols))):
+				raise ValueError('Invalid tuple words')
+			self.symbols = list(range(len(symbols)))
+		elif isinstance(self.lexicon[0], str):
+			self.symbols = list(set(''.join(self.lexicon)))
+		else:
+			raise ValueError('Each word should be string or tuple')
+		self.alphabet_size = len(self.symbols)
+
+		if alpha < 1 / self.alphabet_size or alpha >= 1:
+			raise ValueError('alpha must be >= 1/|S| and < 1')
+		if beta <= 0:
+			raise ValueError('beta must be > 0')
+		if gamma < 1:
+			raise ValueError('gamma must be >= 1')
+
+		self.phi = np.zeros((self.word_length, self.word_length), dtype=float)
+		for fixation_position in range(self.word_length):
+			for position in range(self.word_length):
+				if position < fixation_position:
+					self.phi[fixation_position, position] = (
+						(alpha * self.alphabet_size - 1) * np.exp(-beta * gamma * (fixation_position - position)) + 1
+					) / self.alphabet_size
+				else:
+					self.phi[fixation_position, position] = (
+						(alpha * self.alphabet_size - 1) * np.exp(-beta * (position - fixation_position)) + 1
+					) / self.alphabet_size
+
+		self.p_match = self.phi.copy()
+		self.p_mismatch = (1 - self.phi) / (self.alphabet_size - 1)
+
+		self.percept_type_cache = {}
+
+	def _choose_alternative_character(self, character):
+		'''
+
+		Given a character, return one of the other |S|-1 characters at random.
+
+		'''
+		correct_symbol_index = self.symbols.index(character)
+		perceived_symbol_index = np.random.randint(self.alphabet_size - 1)
+		if perceived_symbol_index >= correct_symbol_index:
+			perceived_symbol_index += 1
+		return self.symbols[perceived_symbol_index]
+
+	def _create_percept(self, target_word, fixation_position):
+		'''
+
+		Create a percept of the target word by adding random noise based on the
+		position of the fixation.
+
+		'''
+		percept = []
+		for position, character in enumerate(target_word):
+			if np.random.random() < self.phi[fixation_position, position]:
+				percept.append(character)
+			else:
+				percept.append(self._choose_alternative_character(character))
+		return percept
+
+	def _create_percept_from_type(self, target_word, percept_type):
+		'''
+
+		Create a percept of the target word that is of some specified percept type.
+
+		'''
+		percept = []
+		for success, character in zip(percept_type, target_word):
+			if success:
+				percept.append(character)
+			else:
+				percept.append(self._choose_alternative_character(character))
+		return percept
+
+	def _likelihood_percept(self, percept, word, fixation_position):
+		'''
+
+		Calculate Pr(p|w,j) - the likelihood of a percept, given that the true
+		target was indeed some hypothesized word fixated in some position.
+		
+		'''
+		likelihood = 1.0
+		for position in range(self.word_length):
+			if percept[position] == word[position]:
+				likelihood *= self.p_match[fixation_position, position]
+			else:
+				likelihood *= self.p_mismatch[fixation_position, position]
+		return likelihood
+
+	def _likelihood_percept_type(self, percept_type, fixation_position):
+		'''
+
+		Calculate Pr(p|w,j) using the percept's type (which is the same value
+		regardless of w).
+		
+		'''
+		likelihood = 1.0
+		for position, success in enumerate(percept_type):
+			if success:
+				likelihood *= self.p_match[fixation_position, position]
+			else:
+				likelihood *= self.p_mismatch[fixation_position, position]
+		return likelihood
+
+	def _iter_percept_types(self, fixation_position):
+		'''
+
+		Iterate over percept types and their likelihoods. This information is cached
+		the first time it's computed.
+
+		'''
+		if fixation_position not in self.percept_type_cache:
+			percept_type_likelihoods = {}
+			for percept_type in product((True, False), repeat=self.word_length):
+				n_percepts_of_this_type = (self.alphabet_size - 1) ** (self.word_length - sum(percept_type))
+				likelihood_percept = self._likelihood_percept_type(percept_type, fixation_position)
+				likelihood_percept_type = likelihood_percept * n_percepts_of_this_type
+				percept_type_likelihoods[percept_type] = likelihood_percept_type
+			self.percept_type_cache[fixation_position] = percept_type_likelihoods
+		for percept_type, likelihood_percept_type in self.percept_type_cache[fixation_position].items():
+			yield percept_type, likelihood_percept_type
+
+	def _posterior_given_percept(self, percept, fixation_position):
+		'''
+
+		Calculate the posterior probability of each word given a percept fixated in
+		a particular position.
+
+		'''
+		likelihood = np.zeros(self.lexicon_size, dtype=float)
+		for w, word in enumerate(self.lexicon):
+			likelihood[w] = self._likelihood_percept(percept, word, fixation_position)
+		posterior = likelihood * self.prior
+		return posterior / posterior.sum()
+
+	def _sample_from_posterior(self, posterior):
+		'''
+
+		Sample an index from a probability distribution.
+		
+		'''
+		random_prob = np.random.random()
+		summation = posterior[0]
+		for inferred_word in range(1, len(posterior)):
+			if random_prob < summation:
+				return inferred_word - 1
+			summation += posterior[inferred_word]
+		return inferred_word
+
+	def read(self, target_word, fixation_position, return_index=False, verbose=False):
+		'''
+
+		Read a target word at a fixation position. If return_index is True, the
+		index of the inferred word is returned rather than the word itself.
+
+		'''
+		percept = self._create_percept(target_word, fixation_position)
+		posterior = self._posterior_given_percept(percept, fixation_position)
+		inferred_word = self._sample_from_posterior(posterior)
+		if return_index:
+			return inferred_word
+		if verbose:
+			print(f'   Target: {target_word}')
+			print(' '*(11+fixation_position) + '^')
+			print(f'  Percept: {"".join(percept)}')
+			print(f'Inference: {self.lexicon[inferred_word]}')
+		return self.lexicon[inferred_word]
+
+	def read_item(self, target_word, fixation_position, return_index=True):
+		'''
+
+		Same as the read() method, except this takes the index of one of the
+		reader's lexical items rather than an item itself. If return_index is True,
+		the index of the inferred word is returned rather than the word itself.
+		
+		'''
+		target_word = self.lexicon[target_word]
+		return self.read(target_word, fixation_position, return_index)
+
+	def test(self):
+		'''
+
+		Test the reader on each word in each fixation position and return the
+		reader's responses.
+
+		'''
+		responses = []
+		for target_word in range(self.lexicon_size):
+			for fixation_position in range(self.word_length):
+				inferred_word = self.read_item(target_word, fixation_position)
+				responses.append((target_word, fixation_position, inferred_word))
+		return responses
+
+	def calculate_posterior(self, target_word, fixation_position):
+		'''
+
+		Calculate the distribution Pr(w|t,j) exactly. Since this is intractable for
+		even moderately sized lexicons, estimate_posterior() or simulate_posterior()
+		should be used instead.
+
+		'''
+		target_word = self.lexicon[target_word]
+		posterior_given_target = np.zeros(self.lexicon_size, dtype=float)
+		for i, percept in enumerate(product(self.symbols, repeat=self.word_length)):
+			likelihood_percept = self._likelihood_percept(percept, target_word, fixation_position)
+			posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
+			posterior_given_target += likelihood_percept + posterior_given_percept
+		return posterior_given_target
+
+	def estimate_posterior(self, target_word, fixation_position):
+		'''
+
+		Estimate the distribution Pr(w|t,j). Similar to calculate_posterior(), but
+		instead of iterating over all possible percepts (|S|^m), we just iterate
+		over types of percept (2^m) and weight the posterior by the number of
+		percepts there are of each type ((|S|-1)^n_failures). This has a similar
+		accuracy to simulate_posterior(), but it's faster.
+
+		'''
+		target_word = self.lexicon[target_word]
+		posterior_given_target = np.zeros(self.lexicon_size, dtype=float)
+		for percept_type, likelihood_percept_type in self._iter_percept_types(fixation_position):
+			percept = self._create_percept_from_type(target_word, percept_type)
+			posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
+			posterior_given_target += likelihood_percept_type * posterior_given_percept
+		return posterior_given_target
+
+	def simulate_posterior(self, target_word, fixation_position, n_sims=10000, non_zero=False):
+		'''
+
+		Estimate the distribution Pr(w|t,j) by simulating a large number of reading
+		trials. If non_zero is set to True, the posterior distribution will not
+		contain any zeros, but this will tend to overestimate the posterior of low
+		probability words, especially if n_sims is not substantially greater than
+		the size of the lexicon.
+		
+		'''
+		if non_zero:
+			posterior_over_words = np.ones(self.lexicon_size, dtype=int)
+			n_sims -= self.lexicon_size
+		else:
+			posterior_over_words = np.zeros(self.lexicon_size, dtype=int)
+		for _ in range(n_sims - self.lexicon_size):
+			inferred_word = self.read_item(target_word, fixation_position)
+			posterior_over_words[inferred_word] += 1
+		return posterior_over_words / n_sims
+
+	def uncertainty(self, target_word, fixation_position):
+		'''
+
+		Calculate the uncertainty (entropy) experienced by the reader when
+		attempting to identify some target in some fixation position.
+
+		'''
+		posterior_over_words = self.estimate_posterior(target_word, fixation_position)
+		return -sum([p * np.log2(p) for p in posterior_over_words if p > 0])
---
+++ code/typ_compute.py
@@ -0,0 +1,130 @@
+'''
+
+This file computes uncertainty by fixation position on the typological corpus
+data. It is designed to be run on a cluster. First you compute the entropy of
+the posterior distribution of each target word in a given fixation position:
+
+	python typ_compute.py --language en --length 5 --position 0
+
+Each fixation position should be run as a separate process. The results will
+periodically be stored in case the process is killed (and these stored results
+should be resurrected automatically).
+
+Once this has been completed for all fixation positions, you can calculate
+uncertainty by fixation position:
+
+	python typ_compute.py --language en --length 5 --merge
+
+In the example above, we are computing the results for 5-letter English words.
+This can then be repeated for each language/length combination. To create
+shell scripts for all language/length combinations:
+
+	python typ_compute.py --script
+
+To check the status of a language/length combination, use:
+
+	python typ_compute.py --check --language en --length 5 
+'''
+
+import pickle
+import model
+
+LEXICON_LOCATION = 'lex/'
+RESULTS_LOCATION = 'res/'
+SLURM_SCRIPT = '''#!/bin/bash
+#SBATCH -N1
+#SBATCH -n1
+#SBATCH --time={time}
+module load python3/3.8
+python typ_compute.py --language {language} --length {length} --position $SLURM_ARRAY_TASK_ID --alpha {alpha} --beta {beta} --gamma {gamma}
+'''
+TIMES = {5:'0:30:00', 7:'2:30:00', 9:'12:00:00'} # e.g. length 5 results require 30mins of compute time
+
+
+def load_lexicon(language, length):
+	with open(LEXICON_LOCATION + f'{args.language}.pkl', 'rb') as file:
+		lexicon_by_length = pickle.load(file)
+	return lexicon_by_length[length]
+
+def read_results(language, length, position):
+	try:
+		with open(RESULTS_LOCATION + f'{language}_{length}_{position}.pkl', 'rb') as file:
+			uncertainty_by_target = pickle.load(file)
+		return uncertainty_by_target
+	except FileNotFoundError:
+		return {}
+
+def write_results(language, length, position, uncertainty_by_target):
+	with open(RESULTS_LOCATION + f'{language}_{length}_{position}.pkl', 'wb') as file:
+		pickle.dump(uncertainty_by_target, file)
+
+def script(languages, lengths, alpha, beta, gamma):
+	for language in languages:
+		for length in lengths:
+			script = SLURM_SCRIPT.format(language=language, length=length, time=TIMES[length], alpha=alpha, beta=beta, gamma=gamma)
+			filename = f'{language}_{length}.sh'
+			with open(filename, 'w') as file:
+				file.write(script)
+			print(f'sbatch --array=0-{length-1} -p regular2 {filename}')
+
+def check(lexicon, language, length):
+	for position in range(length):
+		entropy_by_target = read_results(language, length, position)
+		completed_count = 0
+		for target_word in lexicon:
+			if target_word in entropy_by_target:
+				completed_count += 1
+		percentage = int((completed_count / len(lexicon)) * 100)
+		print(f'{language}_{length}_{position}: {percentage}% complete')
+
+def merge(lexicon, language, length):
+	uncertainty_by_position = []
+	for position in range(length):
+		entropy_by_target = read_results(language, length, position)
+		U = sum([probability * entropy_by_target[target_word] for target_word, probability in lexicon.items()])
+		uncertainty_by_position.append(U)
+	with open(RESULTS_LOCATION + f'{language}_{length}.pkl', 'wb') as file:
+		pickle.dump(uncertainty_by_position, file)
+
+
+if __name__ == '__main__':
+
+	import argparse
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--language', action='store', type=str, help='language code')
+	parser.add_argument('--length', action='store', type=int, help='word length')
+	parser.add_argument('--position', action='store', type=int, help='fixation position')
+
+	parser.add_argument('--alpha', action='store', type=float, default=0.8, help='alpha parameter')
+	parser.add_argument('--beta', action='store', type=float, default=0.25, help='beta parameter')
+	parser.add_argument('--gamma', action='store', type=float, default=1, help='gamma parameter')
+
+	parser.add_argument('--script', action='store_true', help='generate slurm scripts')
+	parser.add_argument('--check', action='store_true', help='print the percentage of targets that have been completed')
+	parser.add_argument('--merge', action='store_true', help='merge the results into a matrix')
+	args = parser.parse_args()
+
+	if args.script:
+		script(['de', 'en', 'es', 'gr', 'it', 'nl', 'pl', 'sw'], [5, 7, 9], args.alpha, args.beta, args.gamma)
+		exit()
+	
+	lexicon = load_lexicon(args.language, args.length)
+
+	if args.check:
+		check(lexicon, args.language, args.length)
+		exit()
+
+	if args.merge:
+		merge(lexicon, args.language, args.length)
+		exit()
+
+	entropy_by_target = read_results(args.language, args.length, args.position)
+	reader = model.Reader(lexicon, args.alpha, args.beta, args.gamma)
+	for t, target_word in enumerate(reader.lexicon):
+		if target_word in entropy_by_target:
+			continue
+		entropy_by_target[target_word] = reader.uncertainty(t, args.position)
+		if t % 100 == 0:
+			write_results(args.language, args.length, args.position, entropy_by_target)
+	write_results(args.language, args.length, args.position, entropy_by_target)
