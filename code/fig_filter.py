05f36c4a62325312cdb69d8757c794aaacd62716
Jon Carr <jwcarr@users.noreply.github.com>
2021-03-11 15:55:56 +0100

    Show gamma in filter figure

--- code/fig_filter.py
+++ code/fig_filter.py
@@ -1,11 +1,12 @@
 from fig import Figure
+import core
 import model
 
-def generate_filter_figure(file_path, lexicon, alpha_vals, beta_vals, gamma):
+def generate_filter_figure(file_path, lexicon, alpha_vals, beta_gamma_vals):
 	alphabet_size = len(set(sum(lexicon, tuple())))
-	fig = Figure(len(alpha_vals)*len(beta_vals), len(beta_vals), figsize=(7.09, 3))
+	fig = Figure(len(alpha_vals)*len(beta_gamma_vals), len(beta_gamma_vals), figsize=(7.09, 2.5))
 	for i, alpha in enumerate(alpha_vals):
-		for j, beta in enumerate(beta_vals):
+		for j, (beta, gamma) in enumerate(beta_gamma_vals):
 			fig[i,j].plot((-1, 5), (1/alphabet_size, 1/alphabet_size), linestyle=':', linewidth=1, color='black')
 			reader = model.Reader(lexicon, alpha, beta, gamma)
 			for dist in reader.phi:
@@ -17,18 +18,20 @@ def generate_filter_figure(file_path, lexicon, alpha_vals, beta_vals, gamma):
 				fig[i,j].set_xticklabels([])
 			else:
 				fig[i,j].set_xticklabels(range(1,6))
-			if j > 0:
-				fig[i,j].set_yticklabels([])
 			if i == len(alpha_vals)-1:
 				fig[i,j].set_xlabel(f'β = {beta}')
 			if j == 0:
 				fig[i,j].set_ylabel(f'α = {alpha}')
+			if i == 0 and (j-1) % 3 == 0:
+				fig[i,j].set_title(f'γ = {gamma}', fontsize=7)
 	fig.save(file_path)
 
 
 lexicon = [(i,) * 5 for i in range(10)]
-alpha_vals = [0.999, 0.8, 0.6, 0.4, 0.2]
-beta_vals = [0.125, 0.25, 0.5, 1, 2]
+alpha_vals = [0.999, 0.8, 0.6]
+beta_gamma_vals = [(0.25, -0.5), (0.5, -0.5), (0.75, -0.5),
+                   (0.25, 0),    (0.5, 0),    (0.75, 0),
+                   (0.25, 0.5),  (0.5, 0.5),  (0.75, 0.5)]
 
-generate_filter_figure(core.FIGS / 'filter.eps', lexicon, alpha_vals, beta_vals, 1)
-generate_filter_figure(core.FIGS / 'filter_asy.eps', lexicon, alpha_vals, beta_vals, 2)
+generate_filter_figure(core.VISUALS / 'filter.pdf', lexicon, alpha_vals, beta_gamma_vals)
+generate_filter_figure(core.FIGS / 'filter.eps', lexicon, alpha_vals, beta_gamma_vals)
