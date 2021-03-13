9b8a1e763cee9fc09f03a52773f23485c0ab4efb
Jon Carr <jwcarr@users.noreply.github.com>
2021-03-13 11:41:35 +0100

    Improve Figure

--- code/fig.py
+++ code/fig.py
@@ -1,39 +1,45 @@
 from pathlib import Path
-import re
-import numpy as np
+
+try:
+	import mplcairo
+	import matplotlib
+	matplotlib.use("module://mplcairo.macosx")
+except:
+	pass
+
 import matplotlib.pyplot as plt
-import cairosvg
+plt.rcParams.update({'font.sans-serif':'Helvetica Neue', 'font.size': 7})
 
 
-plt.rcParams['svg.fonttype'] = 'none'
-plt.rcParams.update({'font.size': 7})
+SINGLE_COLUMN_WIDTH = 3.46 # 88mm
+DOUBLE_COLUMN_WIDTH = 7.09 # 180mm
 
 
 class Figure:
 
-	def __init__(self, n_subplots, n_cols, figsize=None):
+	def __init__(self, n_subplots, n_cols, width='single', height_adjust=1):
 		self.n_cols = n_cols
 		self.n_rows = n_subplots // self.n_cols
 		if n_subplots % self.n_cols > 0:
 			self.n_rows += 1
-		if figsize is None:
-			figsize = self.n_cols * 3, self.n_rows * 2.2
+		if width == 'single':
+			figsize = (SINGLE_COLUMN_WIDTH, SINGLE_COLUMN_WIDTH / self.n_cols * self.n_rows * height_adjust)
+		elif width == 'double':
+			figsize = (DOUBLE_COLUMN_WIDTH, DOUBLE_COLUMN_WIDTH / self.n_cols * self.n_rows * height_adjust)
+		else:
+			figsize = (width, width / self.n_cols * self.n_rows * height_adjust)
 		self.fig, self.axes = plt.subplots(self.n_rows, self.n_cols, figsize=figsize, squeeze=False)
+		self.used = [[False]*self.n_cols for _ in range(self.n_rows)]
 
 	def __getitem__(self, index):
-		return self.axes[index]
+		i, j = index
+		self.used[i][j] = True
+		return self.axes[i,j]
 
 	def __iter__(self):
-		for axis_index in np.ndindex((self.n_rows, self.n_cols)):
-			yield self.axes[axis_index]
-
-	def iter_rows(self):
-		for row_index in range(self.n_rows):
-			yield self.axes[row_index, :]
-
-	def iter_cols(self):
-		for col_index in range(self.n_cols):
-			yield self.axes[:, col_index]
+		for i in range(self.n_rows):
+			for j in range(self.n_cols):
+				yield self[i,j]
 
 	def deduplicate_axes(self):
 		for row in self.axes:
@@ -51,36 +57,19 @@ class Figure:
 				for i in range(self.n_rows-1):
 					col[i].set_xticklabels([])
 
+	def turn_off_unused_axes(self):
+		for i, row in enumerate(self.axes):
+			for j, cell in enumerate(row):
+				if not self.used[i][j]:
+					cell.axis('off')
+
 	def save(self, figure_file, title=None):
 		figure_file = Path(figure_file).resolve()
-		# for remaining_axis in self:
-		# 	remaining_axis.axis('off')
 		self.deduplicate_axes()
+		self.turn_off_unused_axes()
 		if title is not None:
 			self.fig.suptitle(title)
 			self.fig.tight_layout(pad=0.5, h_pad=1, w_pad=1, rect=(0, 0, 1, 0.95))
 		else:
 			self.fig.tight_layout(pad=0.5, h_pad=1, w_pad=1)
-		self.fig.savefig(figure_file, format='svg')
-		self._format_labels(figure_file)
-		if figure_file.suffix == '.pdf':
-			cairosvg.svg2pdf(url=str(figure_file), write_to=str(figure_file))
-		elif figure_file.suffix == '.eps':
-			cairosvg.svg2eps(url=str(figure_file), write_to=str(figure_file))
-		elif figure_file.suffix == '.png':
-			cairosvg.svg2png(url=str(figure_file), write_to=str(figure_file), dpi=300)
-		elif figure_file.suffix != '.svg':
-			raise ValueError('Cannot save to this format. Use either .pdf, .eps, .svg, or .png')
-
-	def _format_labels(self, svg_file_path):
-		'''
-		
-		Applies some nicer formatting to an SVG plot, including setting
-		the font to Helvetica and adding italics.
-
-		'''
-		with open(svg_file_path, mode='r', encoding='utf-8') as file:
-			svg = file.read()
-		svg = re.sub(r'font-family:.*?;', 'font-family:Helvetica Neue;', svg)
-		with open(svg_file_path, mode='w', encoding='utf-8') as file:
-			file.write(svg)
+		self.fig.savefig(figure_file)
