c7de3ff09ea330f8049f09b47b8b680f596ba07d
Jon Carr <jwcarr@users.noreply.github.com>
2021-03-26 19:52:57 +0100

    Combined regular and jitted model code

--- code/model.py
+++ code/model.py
@@ -1,5 +1,6 @@
 from itertools import product
 import numpy as np
+from numba import njit
 
 
 class Reader:
@@ -31,33 +32,41 @@ class Reader:
 		the right. If gamma is 0, the perceptual filter is symmetrical.
 		
 		'''
+		self.lexicon_size = len(lexicon)
 		if isinstance(lexicon, dict):
-			self.lexicon = list(lexicon.keys())
-			self.lexicon_size = len(self.lexicon)
-			self.prior = np.array([lexicon[word] for word in self.lexicon])
+			# separate lexicon and prior
+			lexicon, prior = zip(*lexicon.items())
+			self.prior = np.array(prior, dtype=np.float64)
 		elif isinstance(lexicon, list):
-			self.lexicon = lexicon
-			self.lexicon_size = len(self.lexicon)
-			self.prior = np.array([1 / self.lexicon_size] * self.lexicon_size)
+			# assume uniform prior
+			self.prior = np.full(self.lexicon_size, 1 / self.lexicon_size, dtype=np.float64)
 		else:
 			raise ValueError('lexicon should be of type list or dict')
 		
-		self.word_length = len(self.lexicon[0])
-		for word in self.lexicon:
-			if len(word) != self.word_length:
-				raise ValueError('All words in the lexicon must be of the same length')
+		# check all words are of the same length and type
+		self.word_length = len(lexicon[0])
+		self.word_type = type(lexicon[0])
+		if self.word_type not in [tuple, str]:
+			raise ValueError('Words should be represented as strings or tuples')
+		for word in lexicon:
+			if len(word) != self.word_length or not isinstance(word, self.word_type):
+				raise ValueError('All words in the lexicon must be of the same length and type')
 
-		if isinstance(self.lexicon[0], tuple):
-			symbols = set(sum(self.lexicon, tuple()))
-			if symbols != set(range(len(symbols))):
-				raise ValueError('Invalid tuple words')
-			self.symbols = list(range(len(symbols)))
-		elif isinstance(self.lexicon[0], str):
-			self.symbols = list(set(''.join(self.lexicon)))
+		# extract the set of symbols that the words are composed of
+		if self.word_type is tuple:
+			self.original_symbols = sorted(list(set(sum(lexicon, tuple()))))
 		else:
-			raise ValueError('Each word should be string or tuple')
+			self.original_symbols = sorted(list(set(''.join(lexicon))))
+		self.symbols = list(range(len(self.original_symbols)))
 		self.alphabet_size = len(self.symbols)
 
+		# create the internal lexicon – an NxM array of ints
+		self.lexicon = np.zeros((self.lexicon_size, self.word_length), dtype=np.uint8)
+		for w, word in enumerate(lexicon):
+			for i, character in enumerate(word):
+				self.lexicon[w, i] = self.original_symbols.index(character)
+
+		# check the parameters are valid
 		if alpha < 1 / self.alphabet_size or alpha >= 1:
 			raise ValueError('alpha must be >= 1/|S| and < 1')
 		if beta <= 0:
@@ -65,17 +74,36 @@ class Reader:
 		if gamma >= 1 or gamma <= -1:
 			raise ValueError('gamma must be > -1 and < 1')
 
+		# generate the perceptual filter and precompute p_match and p_mismatch
 		chance = 1 / self.alphabet_size
-		self.phi = np.full((self.word_length, self.word_length), chance, dtype=float)
+		self.phi = np.full((self.word_length, self.word_length), chance, dtype=np.float64)
 		for fixation_position in range(self.word_length):
 			for position in range(self.word_length):
 				if position > fixation_position:
 					self.phi[fixation_position, position] += (alpha-chance) * np.exp( beta * (gamma-1) * abs(fixation_position - position)**2)
 				else:
 					self.phi[fixation_position, position] += (alpha-chance) * np.exp(-beta * (gamma+1) * abs(fixation_position - position)**2)
-
 		self.p_match = self.phi
 		self.p_mismatch = (1 - self.phi) / (self.alphabet_size - 1)
+
+	def _transcribe(self, word):
+		'''
+
+		Transcribe from original symbols to internal symbols.
+
+		'''
+		return [self.original_symbols.index(character) for character in word]
+
+	def _back_transcribe(self, word):
+		'''
+
+		Transcribe from internal symbols to original symbols.
+
+		'''
+		if self.word_type is tuple:
+			return tuple([self.original_symbols[character] for character in word])
+		else:
+			return ''.join([self.original_symbols[character] for character in word])
 
 	def _choose_alternative_character(self, character):
 		'''
@@ -83,17 +111,16 @@ class Reader:
 		Given a character, return one of the other |S|-1 characters at random.
 
 		'''
-		correct_symbol_index = self.symbols.index(character)
-		perceived_symbol_index = np.random.randint(self.alphabet_size - 1)
-		if perceived_symbol_index >= correct_symbol_index:
-			perceived_symbol_index += 1
-		return self.symbols[perceived_symbol_index]
+		perceived_symbol = np.random.randint(self.alphabet_size - 1)
+		if perceived_symbol >= character:
+			perceived_symbol += 1
+		return perceived_symbol
 
 	def _create_percept(self, target_word, fixation_position):
 		'''
 
-		Create a percept of the target word by adding random noise based on the
-		position of the fixation.
+		Create a percept of the target word by adding random noise according to the
+		reader's perceptual filter.
 
 		'''
 		percept = []
@@ -126,86 +153,94 @@ class Reader:
 		a particular position.
 
 		'''
-		likelihood = np.zeros(self.lexicon_size, dtype=float)
+		likelihood = np.zeros(self.lexicon_size, dtype=np.float64)
 		for w, word in enumerate(self.lexicon):
 			likelihood[w] = self._likelihood_percept(percept, word, fixation_position)
 		posterior = likelihood * self.prior
-		return posterior / posterior.sum()
+		return posterior / posterior.sum() # normalize
 
-	def read(self, target_word, fixation_position, return_index=False, verbose=False):
+	def get_word_from_index(self, word_index):
 		'''
 
-		Read a target word at a fixation position. If return_index is True, the
-		index of the inferred word is returned rather than the word itself.
+		Get the word (in original symbols) for a given word index.
 
 		'''
-		percept = self._create_percept(target_word, fixation_position)
+		return self._back_transcribe(self.lexicon[word_index])
+
+	def get_index_from_word(self, word):
+		'''
+
+		Get the index for some word (expressed in original symbols).
+
+		'''
+		search_word = self._transcribe(word)
+		for w, word in enumerate(self.lexicon):
+			n_matches = sum([search_char == char for search_char, char in zip(search_word, word)])
+			if n_matches == self.word_length:
+				return w
+		raise ValueError('This word is not in my lexicon')
+
+	def read(self, target_word, fixation_position):
+		'''
+
+		Read a target word at a fixation position and show the reader's percept and
+		inference. This is mostly intended for playing around with the model.
+
+		'''
+		if len(target_word) != self.word_length:
+			raise ValueError(f'I can only read words of length {self.word_length}')
+		if fixation_position >= self.word_length:
+			raise ValueError('The specified fixation_position is beyond my word length')
+		percept = self._create_percept(self._transcribe(target_word), fixation_position)
 		posterior = self._posterior_given_percept(percept, fixation_position)
 		inferred_word = roulette_wheel(posterior)
-		if verbose:
-			print(f'   Target: {target_word}')
-			print(' Fixation: ' + ' '*fixation_position + '^')
-			print(f'  Percept: {"".join(percept)}')
-			print(f'Inference: {self.lexicon[inferred_word]}')
-		if return_index:
-			return inferred_word
-		return self.lexicon[inferred_word]
+		print(f'   Target (t): {target_word}')
+		if self.word_type is tuple:
+			print(f' Fixation (j): {fixation_position}')
+		else:
+			print(' Fixation (j): ' + ' '*fixation_position + '^')
+		print(f'  Percept (p): {self._back_transcribe(percept)}')
+		print(f'Inference (w): {self._back_transcribe(self.lexicon[inferred_word])}')
 
 	def test(self):
 		'''
 
 		Test the reader on each word in each fixation position and return the
-		reader's responses.
+		reader's responses as a dataset.
 
 		'''
 		responses = []
 		for target_word in range(self.lexicon_size):
 			for fixation_position in range(self.word_length):
-				inferred_word = self.read(self.lexicon[target_word], fixation_position, return_index=True)
+				percept = self._create_percept(self.lexicon[target_word], fixation_position)
+				posterior = self._posterior_given_percept(percept, fixation_position)
+				inferred_word = roulette_wheel(posterior)
 				responses.append((target_word, fixation_position, inferred_word))
 		return responses
 
-	def p_word_given_target(self, target_word, fixation_position, n_sims=10000):
-		'''
-
-		Calculate the distribution Pr(w|t,j) – the probability of the reader
-		inferring some word given some target in some fixation position. A larger
-		number of simulations will produce a more accurate estimate of the
-		distribution. If n_sims is set to 0, an exact calculation is performed by
-		checking all possible percepts (this is intractable for even moderate word
-		lengths and alphabet sizes, and should therefore only be used for test
-		purposes).
-
-		'''
-		p_word_given_target = np.zeros(self.lexicon_size, dtype=float)
-
-		if n_sims == 0:
-			for percept in product(self.symbols, repeat=self.word_length):
-				likelihood_percept = self._likelihood_percept(percept, target_word, fixation_position)
-				posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
-				p_word_given_target += likelihood_percept * posterior_given_percept
-			return p_word_given_target
-
-		for _ in range(n_sims):
-			percept = self._create_percept(target_word, fixation_position)
-			posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
-			p_word_given_target += posterior_given_percept
-		return p_word_given_target / n_sims
-
-	def uncertainty(self, fixation_position, n_sims=10000):
+	def uncertainty(self, fixation_position, method='standard', n_sims=1000):
 		'''
 
 		Calculate the uncertainty (expected entropy of the posterior) experienced by
-		the reader when fixating in a particular fixation position. A larger number
-		of simulations will produce a more accurate estimate of uncertainty. If
-		n_sims is set to 0, an exact calculation is performed by checking all
-		possible percepts (this is intractable for even moderate word lengths and
-		alphabet sizes, and should therefore only be used for test purposes).
+		the reader when fixating in a particular fixation position. There are
+		three methods:
+
+		exhaustive: Perform the calculation using all possible percepts. This gives
+		an exact deterministic result, but it's intractable for even moderate word
+		lengths and alphabet sizes. This is mainly useful for small test cases.
+
+		standard: Perform the calculation by simulating some number of reading
+		events as specified by n_sims.
+
+		fast: Same as standard, except the computation is performed using
+		JIT-compiled code. This is much faster, but the code is less readable.
 
 		'''
-		uncertainty = 0
+		if fixation_position >= self.word_length:
+			raise ValueError('The specified fixation_position is beyond my word length')
 
-		if n_sims == 0:
+		if method == 'exhaustive':
+			uncertainty = 0
 			for target_word, p_target in zip(self.lexicon, self.prior):
 				for percept in product(self.symbols, repeat=self.word_length):
 					likelihood_percept = self._likelihood_percept(percept, target_word, fixation_position)
@@ -213,12 +248,65 @@ class Reader:
 					uncertainty += p_target * likelihood_percept * entropy(posterior_given_percept)
 			return uncertainty
 
-		for target_word, p_target in zip(self.lexicon, self.prior):
+		if method == 'standard':
+			uncertainty = 0
+			for target_word, p_target in zip(self.lexicon, self.prior):
+				for _ in range(n_sims):
+					percept = self._create_percept(target_word, fixation_position)
+					posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
+					uncertainty += p_target * entropy(posterior_given_percept)
+			return uncertainty / n_sims
+
+		if method == 'fast':
+			return jitted_uncertainty(self.lexicon, self.prior, self.phi, fixation_position, n_sims)
+
+		raise ValueError('method should be exhaustive, standard, or fast.')
+
+	def p_word_given_target(self, target_word, fixation_position, method='standard', n_sims=1000):
+		'''
+
+		Calculate the distribution Pr(w|t,j) – the probability of the reader
+		inferring each word given some target in some fixation position. There are
+		three methods:
+
+		exhaustive: Perform the calculation using all possible percepts. This gives
+		an exact deterministic result, but it's intractable for even moderate word
+		lengths and alphabet sizes. This is mainly useful for small test cases.
+
+		standard: Perform the calculation by simulating some number of reading
+		events as specified by n_sims.
+
+		fast: Same as standard, except the computation is performed using
+		JIT-compiled code. This is much faster, but the code is less readable.
+
+		'''
+		if not isinstance(target_word, int) or target_word >= self.lexicon_size:
+			raise ValueError('The target word should be specified as an index')
+		if fixation_position >= self.word_length:
+			raise ValueError('The specified fixation_position is beyond my word length')
+
+		target_word = self.lexicon[target_word]
+
+		if method == 'exhaustive':
+			p_word_given_target = np.zeros(self.lexicon_size, dtype=np.float64)
+			for percept in product(self.symbols, repeat=self.word_length):
+				likelihood_percept = self._likelihood_percept(percept, target_word, fixation_position)
+				posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
+				p_word_given_target += likelihood_percept * posterior_given_percept
+			return p_word_given_target
+
+		if method == 'standard':
+			p_word_given_target = np.zeros(self.lexicon_size, dtype=np.float64)
 			for _ in range(n_sims):
 				percept = self._create_percept(target_word, fixation_position)
 				posterior_given_percept = self._posterior_given_percept(percept, fixation_position)
-				uncertainty += p_target * entropy(posterior_given_percept)
-		return uncertainty / n_sims
+				p_word_given_target += posterior_given_percept
+			return p_word_given_target / n_sims
+
+		if method == 'fast':
+			return jitted_p_word_given_target(self.lexicon, self.prior, self.phi, target_word, fixation_position, n_sims)
+
+		raise ValueError('method should be exhaustive, standard, or fast.')
 
 
 def entropy(distribution):
@@ -247,3 +335,102 @@ def roulette_wheel(distribution):
 			return i - 1
 		summation += distribution[i]
 	return i
+
+
+@njit
+def jitted_p_word_given_target(lexicon, prior, phi,
+	target_word, fixation_position, n_sims=1000):
+	'''
+
+	This is the jitted version of Reader.p_word_given_target(). All probability
+	calculations are done in the log domain.
+
+	'''
+	lexicon_size, word_length = lexicon.shape
+	alphabet_size_minus_1 = lexicon.max()
+
+	log_prior = np.log2(prior)
+	phi_given_fixation = phi[fixation_position]
+	log_p_match = np.log2(phi_given_fixation)
+	log_p_mismatch = np.log2((1 - phi_given_fixation) / alphabet_size_minus_1)
+	
+	percept = np.zeros(word_length, dtype=np.uint8)
+	log_posteriors = np.zeros((n_sims, lexicon_size), dtype=np.float64)
+
+	for s in range(n_sims):
+
+		for i in range(word_length):
+			if np.random.random() < phi_given_fixation[i]:
+				percept[i] = target_word[i]
+			else:
+				perceived_symbol = np.random.randint(alphabet_size_minus_1)
+				if perceived_symbol >= target_word[i]:
+					perceived_symbol += 1
+				percept[i] = perceived_symbol
+
+		for w in range(lexicon_size):
+			log_posteriors[s, w] = log_prior[w]
+			for i in range(word_length):
+				if percept[i] == lexicon[w, i]:
+					log_posteriors[s, w] += log_p_match[i]
+				else:
+					log_posteriors[s, w] += log_p_mismatch[i]
+		mx = log_posteriors[s].max()
+		log_posteriors[s] -= np.log2(np.sum(np.exp2(log_posteriors[s] - mx))) + mx
+
+	log_posterior = np.zeros(lexicon_size, dtype=np.float64)
+	for w in range(lexicon_size):
+		mx = log_posteriors[:, w].max()
+		log_posterior[w] = np.log2(np.sum(np.exp2(log_posteriors[:, w] - mx))) + mx
+	return np.exp2(log_posterior - np.log2(n_sims))
+
+
+@njit
+def jitted_uncertainty(lexicon, prior, phi,
+	fixation_position, n_sims=1000):
+	'''
+
+	This is the jitted version of Reader.uncertainty(). All probability
+	calculations are done in the log domain.
+
+	'''
+	lexicon_size, word_length = lexicon.shape
+	alphabet_size_minus_1 = lexicon.max()
+
+	log_prior = np.log2(prior)
+	phi_given_fixation = phi[fixation_position]
+	log_p_match = np.log2(phi_given_fixation)
+	log_p_mismatch = np.log2((1 - phi_given_fixation) / alphabet_size_minus_1)
+	
+	log_posterior = np.zeros(lexicon_size, dtype=np.float64)
+	percept = np.zeros(word_length, dtype=np.uint8)
+	uncertainty = 0.0
+
+	for t in range(lexicon_size):
+		target = lexicon[t]
+		p_target = prior[t]
+
+		for _ in range(n_sims):
+
+			for i in range(word_length):
+				if np.random.random() < phi_given_fixation[i]:
+					percept[i] = target[i]
+				else:
+					perceived_symbol = np.random.randint(alphabet_size_minus_1)
+					if perceived_symbol >= target[i]:
+						perceived_symbol += 1
+					percept[i] = perceived_symbol
+
+			for w in range(lexicon_size):
+				log_posterior[w] = log_prior[w]
+				for i in range(word_length):
+					if percept[i] == lexicon[w, i]:
+						log_posterior[w] += log_p_match[i]
+					else:
+						log_posterior[w] += log_p_mismatch[i]
+			mx = log_posterior.max()
+			log_posterior -= np.log2(np.sum(np.exp2(log_posterior - mx))) + mx
+
+			uncertainty += p_target * -np.sum(np.exp2(log_posterior) * log_posterior)
+
+	return uncertainty / n_sims
