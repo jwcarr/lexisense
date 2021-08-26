f8940cd6083a753c376f2b0e3fb89466a4e6389a
Jon Carr <jwcarr@users.noreply.github.com>
2021-08-26 13:00:29 +0200

    Switch to the surrogate likelihood + PyMC3 paradigm for model fitting

--- code/model_fit.py
+++ code/model_fit.py
@@ -1,27 +1,32 @@
 '''
 
-This module handles fitting experimental data to the model. To fit the model,
-pass an Experiment or Task object to fit_model_to_dataset():
+This model handles performing the model fit for Experiment 1. The general
+usage pattern is to first create the surrogate likelihood from an experiment
+or task object, e.g.:
 
->>> fit_model_to_dataset(experiment, 'path/to/store/results.pkl')
+	create_surrogate_likelihood(experiment, 'likelihood.pkl')
 
-And to print the MAP estimates with 95% credible regions, use:
+and then draw samples from the posterior, e.g.:
 
->>> print_map_estimates('path/to/store/results.pkl')
+	create_posterior_trace('likelihood.pkl', 'posterior.pkl')
+
+The posterior trace can then be inspected using the functions provided in
+exp_analysis.py
 
 '''
 
-
+import pickle
 import numpy as np
 import skopt
+import pymc3
 import model
 
 
-PARAMETER_SPACE = [
-	skopt.space.Real( 0.0625, 0.9999, name='α'),
-	skopt.space.Real( 0.0001, 0.9999, name='β'),
-	skopt.space.Real(-0.9999, 0.9999, name='γ'),
-	skopt.space.Real( 0.0001, 0.9999, name='ε'),
+PARAMETERS = [
+	{'name':'α', 'bounds':( 0.0625, 0.9999), 'prior':(4, 2)},
+	{'name':'β', 'bounds':( 0.0001, 1.0000), 'prior':(2, 8)},
+	{'name':'γ', 'bounds':(-0.9999, 0.9999), 'prior':(2, 2)},
+	{'name':'ε', 'bounds':( 0.0001, 0.9999), 'prior':(2, 8)},
 ]
 
 
@@ -30,11 +35,15 @@ def print_iteration(result, final=False):
 	Skopt callback function for printing result of current iteration.
 	'''
 	if final:
-		parameter_vals = ', '.join([f'{param.name} = {round(value, 4)}' for param, value in zip(PARAMETER_SPACE, result.x)])
+		parameter_vals = ', '.join([f'{param.name} = {round(value, 4)}'
+			for param, value in zip(result.space.dimensions, result.x)
+		])
 		log_likelihood = f'log likelihood = {-result.fun}'
 		print(parameter_vals, log_likelihood)
 	else:
-		parameter_vals = ', '.join([f'{param.name} = {round(value, 4)}' for param, value in zip(PARAMETER_SPACE, result.x_iters[-1])])
+		parameter_vals = ', '.join([f'{param.name} = {round(value, 4)}'
+			for param, value in zip(result.space.dimensions, result.x_iters[-1])
+		])
 		log_likelihood = f'log likelihood = {-result.func_vals[-1]}'
 		if len(result.func_vals) > 1 and result.func_vals[-1] < min(result.func_vals[:-1]):
 			log_likelihood += ' *'
@@ -58,11 +67,13 @@ def compute_p_word_given_target(lexicons, theta, n_words, word_length, n_simulat
 	return word_inference_matrix_for_each_lexicon
 
 
-def fit_model_to_dataset(experiment, output_path, n_evaluations=300, n_random_evaluations=100, n_simulations=100000, tip=None):
+def create_surrogate_likelihood(experiment, surrogate_likelihood_file, n_evaluations=300, n_random_evaluations=200, n_simulations=100000):
 	'''
-	Use the skopt optimizer to the find parameter values that minimize the
-	negative log likelihood of the model generating an observed experimental
-	dataset. The resulting OptimizeResult object is writen to a file.
+	Use Skopt to the find parameter values that minimize the negative log
+	likelihood of the model generating an observed experimental dataset.
+	The resulting OptimizeResult object is writen to a file. The final
+	Gaussian Process model contained in this object is an approximation of
+	the true likelihood function.
 	'''
 	if isinstance(experiment, tuple) and len(experiment) == 2:
 		dataset, lexicons = experiment
@@ -83,180 +94,58 @@ def fit_model_to_dataset(experiment, output_path, n_evaluations=300, n_random_ev
 				# probability of inferring w and sticking to it, plus probability of
 				# inferring some other w' but switching to w by mistake
 				p_word_given_target[l][t, j, w] * p_stick_with_w + sum([
-					p_word_given_target[l][t, j, w_prime] * p_switch_to_w for w_prime in range(8) if w_prime != w
+					p_word_given_target[l][t, j, w_prime] * p_switch_to_w for w_prime in range(n_words) if w_prime != w
 				])
 			)
 		return -log_likelihood
 
 	result = skopt.gp_minimize(
 		neg_log_likelihood_dataset,
-		dimensions=PARAMETER_SPACE,
+		dimensions=[skopt.space.Real(*param['bounds'], name=param['name']) for param in PARAMETERS],
 		n_calls=n_evaluations,
 		n_random_starts=n_random_evaluations,
-		x0=tip,
 		model_queue_size=1,
 		callback=print_iteration,
 	)
 	del result.specs['args']['func']
 	del result.specs['args']['callback']
-	skopt.utils.dump(result, output_path)
+	skopt.utils.dump(result, surrogate_likelihood_file)
 	return result
 
 
-def view_model_fit(file_path):
+class BlackBoxLikelihood(pymc3.utils.tt.Op):
+
+	itypes = [pymc3.utils.tt.dvector]
+	otypes = [pymc3.utils.tt.dscalar]
+
+	def __init__(self, func):
+		self.func = func
+
+	def perform(self, node, inputs, outputs):
+		outputs[0][0] = pymc3.utils.tt.np.array(self.func(inputs[0]))
+
+
+def create_posterior_trace(surrogate_likelihood_file, posterior_trace_file, n_samples=10000, n_tuning_samples=500, n_chains=4):
 	'''
-	Plot model fit results from the OptimizeResult object previously written to a
-	file.
+	Use PyMC3 to draw samples from the posterior. This combines the surrogate
+	likelihood function, created by create_surrogate_likelihood(), with
+	the prior, defined in PARAMETERS, and pickles the trace.
 	'''
-	from skopt.plots import plot_convergence, plot_objective
-	import matplotlib.pyplot as plt
-	result = skopt.utils.load(file_path)
-	print_iteration(result, final=True)
-	plot_objective(result, size=2, levels=32, show_points=False)
-	plt.show()
+	skopt_optimization_result = skopt.utils.load(surrogate_likelihood_file)
+	final_GP_model = skopt_optimization_result.models[-1]
+	surrogate_likelihood = BlackBoxLikelihood(lambda theta: -final_GP_model.predict([theta])[0])
 
+	with pymc3.Model() as model:
+		theta = pymc3.utils.tt.as_tensor_variable([
+			pymc3.Beta(param['name'], *param['prior']) for param in PARAMETERS
+		])
+		pymc3.DensityDist('likelihood', lambda v: surrogate_likelihood(v), observed={'v': theta})
+		trace = pymc3.sample(n_samples, tune=n_tuning_samples, chains=n_chains, cores=1,
+			return_inferencedata=True, idata_kwargs={'density_dist_obs': False}
+		)
 
-def metropolis(evaluation_func, n_params, mcmc_samples, burn_in, sd=0.01):
-	'''
-	Draw samples from a log2 probability function using the Metropolis
-	algorithm. evaluation_func should expect params to be scaled in [0, 1].
-	'''
-	samples = np.empty((mcmc_samples, n_params + 1))
-	params = [0.5] * n_params # initialize at center of param space
-	log_p = evaluation_func(params)
-	for i in range(mcmc_samples + burn_in):
-		candidate_params = [np.clip(np.random.normal(param, sd), 0, 1) for param in params]
-		candidate_log_p = evaluation_func(candidate_params)
-		alpha = candidate_log_p - log_p # acceptance ratio
-		if (alpha >= 0.0) or (np.log2(np.random.random()) < alpha):
-			# candidate accepted
-			params = candidate_params
-			log_p = candidate_log_p
-		if i >= burn_in:
-			samples[i - burn_in] = [*params, log_p]
-	return samples
+	for dim in skopt_optimization_result.space.dimensions:
+		trace.posterior.data_vars[dim.name][:] = dim.inverse_transform(trace.posterior.data_vars[dim.name][:])
 
-
-def logsumexp2(array):
-	'''
-	Sum an array in the log2 domain.
-	'''
-	array_max = array.max()
-	return np.log2(np.sum(np.exp2(array - array_max))) + array_max
-
-
-def sample_posterior(result, mcmc_samples, burn_in, thin=None):
-	'''
-	Draw samples from the final model in a skopt optimization result object.
-	'''
-	try:
-		n_params = len(result.space.dimensions)
-	except:
-		result = skopt.utils.load(result)
-		n_params = len(result.space.dimensions)
-	GP_model = result.models[-1] # final surrogate is approximation of true posterior
-	evaluation_func = lambda params: -GP_model.predict([params])[0]
-	samples = metropolis(evaluation_func, n_params, mcmc_samples, burn_in)
-	if thin:
-		# Reduce number of samples to some target number of samples
-		samples = samples[np.random.choice(mcmc_samples, thin, replace=False)]
-	# Transform param values back to original space
-	for i, dim in enumerate(result.space.dimensions):
-		samples[:, i] = dim.inverse_transform(samples[:, i])
-	return samples
-
-
-def maximum_a_posteriori(samples):
-	'''
-	Calculate the MAP estimates from some MCMC samples.
-	'''
-	return samples[np.argmax(samples[:, -1]), :-1]
-
-
-def highest_posterior_density(samples, probability=0.95):
-	'''
-	Calculate HPD credible regions from some MCMC samples for a given
-	probability.
-	'''
-	samples[:, -1] -= logsumexp2(samples[:, -1]) # normalize probs
-	probability = np.log2(probability)
-	sum_p = None
-	params_in_hdi = []
-	# Iterate over samples from most probable to least probable
-	for *params, p in sorted(samples, key=lambda r: r[-1], reverse=True):
-		sum_p = p if sum_p is None else np.logaddexp2(sum_p, p)
-		params_in_hdi.append(params)
-		if sum_p >= probability:
-			break # stop once x% of the probability mass has been accounted for
-	params_in_hdi = np.array(params_in_hdi)
-	return list(zip(np.min(params_in_hdi, axis=0), np.max(params_in_hdi, axis=0)))
-
-
-def print_map_estimates(result_file, credible_regions=0.95, round_to=2, mcmc_samples=10000, burn_in=300):
-	result = skopt.utils.load(result_file)
-	samples = sample_posterior(result, mcmc_samples, burn_in)
-	map_estimates = maximum_a_posteriori(samples)
-	credible_regions = highest_posterior_density(samples, credible_regions)
-	for dim, estimate, (lower, upper) in zip(result.space.dimensions, map_estimates, credible_regions):
-		estimate = round(estimate, round_to)
-		lower = round(lower, round_to)
-		upper = round(upper, round_to)
-		print(f'{dim.name} = {estimate} ({lower}---{upper})')
-
-
-def extract_slice_from_gaussian_process(result, target_param, granularity=1000, return_log=False):
-	'''
-	Extract the posterior over parameter values for a particular target
-	parameter from a skopt optimization result. This effectively takes a
-	slice through the Gaussian process landscape along one dimension,
-	while holding all other dimensions constant at the MAP estimate.
-	'''
-	param_bounds = result.space.dimensions[target_param].bounds
-	param_space = np.linspace(*param_bounds, granularity)
-	
-	# Holding all other parameters constant at the MAP, extract the shape
-	# of the Gaussian process along the target parameter
-	GP_input = np.empty((granularity, len(result.x)))
-	for param, param_value in enumerate(result.x):
-		dimension = result.space.dimensions[param]
-		if param == target_param:
-			GP_input[:, param] = dimension.transform(param_space)
-		else:
-			GP_input[:, param] = dimension.transform(param_value)
-	GP_prediction = result.models[-1].predict(GP_input)
-	
-	# Convert the Gaussian process predictions into posterior estimates
-	log_posterior = -GP_prediction # un-negative the predictions
-	log_posterior = log_posterior - logsumexp2(log_posterior) # normalize
-	# log_posterior = log_posterior - (-result.fun) # alternative normalize style
-	if return_log:
-		return param_space, log_posterior
-	return param_space, np.exp2(log_posterior)
-
-
-if __name__ == '__main__':
-
-	# Test the model fit procedure by generating a synthetic dataset and
-	# attempting to recover the underlying parameters.
-
-	lexicons = [
-		[(0, 6, 12, 2, 10, 1, 0), (0, 7, 13, 2, 10, 1, 0), (0, 8, 14, 3, 10, 1, 0), (0, 9, 15, 3, 10, 1, 0), (0, 8, 12, 4, 11, 1, 0), (0, 6, 13, 4, 11, 1, 0), (0, 9, 14, 5, 11, 1, 0), (0, 7, 15, 5, 11, 1, 0)],
-		[(0, 1, 10, 2, 12, 6, 0), (0, 1, 10, 2, 13, 7, 0), (0, 1, 10, 3, 14, 8, 0), (0, 1, 10, 3, 15, 9, 0), (0, 1, 11, 4, 12, 8, 0), (0, 1, 11, 4, 13, 6, 0), (0, 1, 11, 5, 14, 9, 0), (0, 1, 11, 5, 15, 7, 0)]
-	]
-	
-	dataset = simulate_experimental_dataset(
-		lexicons,
-		params=[0.8, 0.1, 0.3, 0.05],
-		n_participants=[30, 30],
-	)
-	
-	fit_model_to_dataset(
-		(dataset, lexicons),
-		output_path='test.pkl',
-		n_evaluations=100,
-		n_random_evaluations=30,
-		n_simulations=100000,
-		tip=[0.8, 0.15, 0.4, 0.1]
-	)
-
-	print_map_estimates('test.pkl')
+	with open(posterior_trace_file, 'wb') as file:
+		pickle.dump((trace, PARAMETERS), file)
